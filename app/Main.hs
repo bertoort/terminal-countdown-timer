4590e6b3e4f518dcbe5ebb2d863c9fefee56d0b7
Roberto Ortega <berto.ort@gmail.com>
2017-01-20 10:26:14 -0700

    ✨ Add argument parser for time

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,23 @@
 module Main where
 
 import Timer
+import Options.Applicative
+
+data ArgParser = ArgParser
+  { argtime :: String }
+
+parser :: Parser ArgParser
+parser = ArgParser
+     <$> argument str (metavar "[time]")
+
+countdown :: ArgParser -> IO ()
+countdown (ArgParser t) = putStrLn $ convert t
+countdown _ = return ()
 
 main :: IO ()
-main = putStrLn (convert ", this is a start")
+main = execParser opts >>= countdown
+  where
+    opts = info (helper <*> parser)
+      ( fullDesc
+     <> progDesc "Starts timer with the given time"
+     <> header "timer - a countdown timer for the terminal" )
