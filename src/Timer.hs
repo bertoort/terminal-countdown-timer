d89e4cc4293c6a7a6ba81e06e7b50c788c1de4b9
Roberto Ortega <berto.ort@gmail.com>
2017-01-21 17:46:45 -0700

    ☑️ Pass formatting time test

--- src/Timer.hs
+++ src/Timer.hs
@@ -2,5 +2,38 @@ module Timer
     ( convert
     ) where
 
+import Data.List
+
+data InvalidNumber = InvalidNumber Int String
+
+instance Show InvalidNumber where
+    show (InvalidNumber num err) = show num ++ " - " ++ err
+
 convert :: [Char] -> [Char]
-convert num = num ++ "ok" 
+convert num 
+    | len > 4 = "error" 
+    | len == 0 = "error" 
+    | not $ validate list = "error" 
+    | otherwise = display list
+    where len = length num
+          list = format num
+
+format :: [Char] -> [Int] 
+format num = do
+    let list = map (read . (:"")) num :: [Int]
+    lpad 4 list
+
+lpad :: Int -> [Int] -> [Int]
+lpad m xs = replicate (m - length ys) 0 ++ ys
+    where ys = take m xs
+
+display :: [Int] -> [Char]
+display xs = do
+    let (ys,zs) = splitAt 2 xs
+    join ys ++ ":" ++ join zs 
+
+join :: [Int] -> [Char]
+join xs = intercalate "" $ map show xs
+
+validate :: [Int] -> Bool 
+validate list = True 
